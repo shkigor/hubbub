532a901e3beec38ae4595ef1ee07d7a9cf1a4972
Igor Shklyar <igor.shklyar@gmail.com>
2015-10-19 23:07:42 +0300

    one-to-many (page 84)

---
+++ test/integration/com/grailsinaction/PostIntegrationSpec.groovy
@@ -0,0 +1,15 @@
+package com.grailsinaction
+
+import grails.test.spock.IntegrationSpec
+
+class PostIntegrationSpec extends IntegrationSpec {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
